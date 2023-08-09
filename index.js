ec99e16e13ecec3e048bac51b99ea1113694adc1
bhavesha <bhavesha.talreja@gmail.com>
2023-08-10 01:19:47 +0530

    product page added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 var http = require("http");
+var url = require("url");
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -37,7 +38,9 @@ const convertTemplate = (temp, product) => {
 };
 
 const server = http.createServer((req, res) => {
-  const pathname = req.url;
+  //console.log(req.url);
+  const { query, pathname } = url.parse(req.url, true);
+  console.log("after url parse");
 
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -46,11 +49,17 @@ const server = http.createServer((req, res) => {
       .map((el) => convertTemplate(tempCard, el))
       .join("");
     const displayOutput = tempOverview.replace("{%PRODUCTS_CARDS%}", cardsHTML);
-    console.log(displayOutput);
+    //console.log(displayOutput);
 
     res.end(displayOutput);
-  } else if (pathname === "/question") {
-    res.end("This is question");
+    //product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    const product = dataObj[query.id];
+    const output = convertTemplate(tempProduct, product);
+    res.end(output);
+
+    //api page
   } else if (pathname === "/api") {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(data);
